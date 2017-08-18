288d20d61065be7f293d7af7efc7c709022e4bac
John Tseng <john@jtsengsolutions.com>
2017-08-18 16:25:05 -0500

    add DbEntity

---
+++ AspNetCoreUtilities/DbEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AspNetCoreUtilities
+{
+    public abstract class DbEntity
+    {
+        public long Id { get; set; }
+    }
+
+    public abstract class DbSoftDeleteEntity :DbEntity
+    {
+        public bool IsDeleted { get; set; }
+    }
+
+    public static class DbEntityExtensions
+    {
+        public static IQueryable<T> NotDeleted<T>(IQueryable<T> queryable)
+            where T : DbSoftDeleteEntity =>
+            queryable.Where(e => !e.IsDeleted);
+    }
+}
